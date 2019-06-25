f29dcfa8e0ab14befd8365d514155114efffa396
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2019-06-25 17:07:36 +0000

    configuration openMPI

---
+++ Monte Carlo/test.sh
@@ -0,0 +1,4 @@
+export PATH="$PATH:/usr/local/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/"
+echo export PATH="$PATH:/usr/local/bin" >> /home/$USER/.bashrc
+echo export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/" >> /home/$USER/.bashrc
